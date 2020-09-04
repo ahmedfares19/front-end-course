9c0c4908646bcb0e72a91f16e81abf22da13c0a3
Ahmed Fares <afares@arabdt.com>
2020-09-04 14:22:41 +0200

    day6

---
+++ front-end/day9/addnumbers/script.js
@@ -0,0 +1,28 @@
+function addNumbers(numberOne,numberTwo){
+    return numberOne  +  numberTwo
+}
+
+
+ const numberOneInput = document.getElementById('num1').value
+ const numberTwoInput = document.getElementById('num2').value
+ const addBtn = document.getElementById('add-btn')
+
+ 
+addBtn.onclick = () => {
+
+}
+
+console.log(numberOneInput);
+console.log(numberTwoInput);
+
+
+// myFunction()
+// myFunctionTwo();
+// function myFunctionTwo() {
+//     console.log('from my function two');   
+// }
+
+// myFunction = () => {
+// console.log("from my function");
+// }
+
---
+++ front-end/day9/main.js
@@ -0,0 +1,15 @@
+// function addNumbers(number1, number2) {
+//     sum = number1 + number2
+//   console.log(sum);
+// }
+
+// addNumbers(10, 10);
+
+
+
+
+// console.log(document.getElementsByTagName('h2'));
+
+const tag = document.getElementById('test')
+console.log(tag);
+console.log(document.querySelector('#test'));
